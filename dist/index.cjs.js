77a9f5e1e5b6079cf1b69d6f031ec30e97a16d8e
awefeng <awefeng@163.com>
2022-06-23 23:30:01 +0800

    feat: 修改rollup配置 & 打包配置

---
+++ dist/index.cjs.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function add(x, y) {
+    return x + y;
+}
+
+var HOURS_PER_DAY = 24;
+var say = function (str) {
+    return str;
+};
+
+exports.HOURS_PER_DAY = HOURS_PER_DAY;
+exports.add = add;
+exports.say = say;
